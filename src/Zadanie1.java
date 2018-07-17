ef09a7a402a3687ac3fbfcdb0c3acc7951709563
Agata <twardowska123@gmail.com>
2018-07-17 10:32:07 +0200

    zadania1

---
+++ src/Zadanie1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+        names[0] = "Agata";
+        names[1] = "Kasia";
+        names[2] = "Artur";
+
+        String arrayString = Arrays.toString(names);
+        System.out.println(arrayString);
+
+    }
+
+
+}
