5c35159668ed2b0171c2f015023a0fffb084a82a
Agata <twardowska123@gmail.com>
2018-07-17 11:48:52 +0200

    poprawki

--- src/Zadanie5.java
+++ src/Zadanie5.java
@@ -7,17 +7,17 @@ public class Zadanie5 {
         while (i <= 100) {
 
             if (i % 2 != 0) {
-                i++;
-                continue;
+                i = i + 2;
+
             }
             System.out.println(i);
-            i++;
+            i = i + 2;
 
         }
 
-        for (i = 1; i <= 100; i++)
+        for (i = 1; i <= 100; i = i+1)
             if (i % 2 == 0)
-                sum = sum + i++;
+                sum = sum + i;
 
         System.out.println("Suma liczb parzystych:" + sum);
 
