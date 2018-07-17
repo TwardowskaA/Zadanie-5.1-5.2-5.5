102174f5c7759ae07488a680efb710214d37706c
Agata <twardowska123@gmail.com>
2018-07-17 10:32:24 +0200

    zadania1

---
+++ src/Zadanie2.java
@@ -0,0 +1,17 @@
+public class Zadanie2 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = 2;
+        numbers[1] = 4;
+        numbers[2] = 6;
+        numbers[3] = 8;
+        numbers[4] = 10;
+
+        int sum = numbers[0] + numbers[2] + numbers[4];
+
+        System.out.println(numbers[0] + "+" + numbers[2] + "+" + numbers[4] + "=" + sum);
+    }
+
+}
